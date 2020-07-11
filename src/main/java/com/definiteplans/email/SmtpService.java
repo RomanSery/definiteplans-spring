e9ff8540eace07ca0546092ccf6c78c97fd193f6
Roman Sery <roman.sery@novelution.com>
2020-07-11 16:44:14 -0400

    use sendgrid

--- src/main/java/com/definiteplans/email/SmtpService.java
+++ src/main/java/com/definiteplans/email/SmtpService.java
@@ -14,6 +14,14 @@ import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 import com.definiteplans.dao.SentEmailRepository;
 import com.definiteplans.dom.SentEmail;
 import com.definiteplans.util.DateUtil;
+import com.definiteplans.util.Utils;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 
 import freemarker.template.Configuration;
 import freemarker.template.Template;
@@ -24,37 +32,61 @@ import freemarker.template.TemplateExceptionHandler;
 public class SmtpService {
     private static final Logger logger = LoggerFactory.getLogger(SmtpService.class);
 
-    private static final String from = "noreply@letsdefinitly.com";
+    private static final String fromEmail = "roman@letsdefinitely.com";
 
     private final JavaMailSender sender;
     private final SentEmailRepository sentEmailRepository;
     private final Configuration freeMarkerConfiguration;
 
+    private SendGrid sendGrid;
+
     public SmtpService(JavaMailSender sender, SentEmailRepository sentEmailRepository, Configuration freeMarkerConfiguration) {
         this.sender = sender;
         this.sentEmailRepository = sentEmailRepository;
         this.freeMarkerConfiguration = freeMarkerConfiguration;
+
+        String apiKey = Utils.getVariable("SENDGRID_API_KEY");
+        if(!StringUtils.isBlank(apiKey)) {
+            sendGrid = new SendGrid(apiKey);
+        }
     }
 
 
     public void sendEmail(String templateFile, String subject, String to, Map<String, String> ctx) throws Exception {
-        MimeMessage message = sender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message);
 
         String body = doTemplatingForContent(templateFile, ctx);
 
-        helper.setSubject(subject);
-        helper.setFrom(from);
-        helper.setTo(to);
-        helper.setText(body, false);
+        if(sendGrid != null) {
+            sendEmailWithSendGrid(body, subject, to);
+        } else {
+            MimeMessage message = sender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message);
+            helper.setSubject(subject);
+            helper.setFrom(fromEmail);
+            helper.setTo(to);
+            helper.setText(body, false);
+            sender.send(message);
+        }
 
-        sender.send(message);
-
-        SentEmail email = new SentEmail(subject, body, from, to);
+        SentEmail email = new SentEmail(subject, body, fromEmail, to);
         email.setEmailSentDate(DateUtil.now());
         sentEmailRepository.save(email);
     }
 
+    private void sendEmailWithSendGrid(String body, String subject, String to) throws Exception {
+
+        Email from = new Email(fromEmail);
+        Email toEmail = new Email(to);
+        Content content = new Content("text/plain", body);
+        Mail mail = new Mail(from, subject, toEmail, content);
+
+        Request request = new Request();
+        request.setMethod(Method.POST);
+        request.setEndpoint("mail/send");
+        request.setBody(mail.build());
+        Response response = sendGrid.api(request);
+        logger.info("sent email: code: {} body: {}", response.getStatusCode(), response.getBody());
+    }
 
 
     private String doTemplatingForContent(String templateFile, Map<String,?> ctx) {
